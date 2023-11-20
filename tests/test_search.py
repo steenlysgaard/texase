5fdf05801d285181ae48f2e495ad19a07b85ef11
Steen Lysgaard <stly@dtu.dk>
2023-11-20 14:46:48 +0100

    initial test_search

--- tests/test_search.py
+++ tests/test_search.py
@@ -10,6 +10,12 @@ from .shared_info import test_atoms, get_column_labels
 
 
 @pytest.mark.asyncio
-async def test_filter(db_path):
-    app = ASETUI(path=db_path)
+async def test_search(big_db_path):
+    app = ASETUI(path=big_db_path)
     async with app.run_test(size=(200, 50)) as pilot:
+        searchbox = app.query_one("#search-box")
+        
+        # Check status before adding filter
+        assert not app.show_search_box
+        assert not searchbox.display
+        
