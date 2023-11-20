246757afda13a21974c420b8295ce834277ae9e6
Steen Lysgaard <stly@dtu.dk>
2023-11-20 15:29:34 +0100

    expand search test

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,12 +1,23 @@
 import pytest
 
-from textual.widgets._data_table import RowKey
+from textual.coordinate import Coordinate
+
+from ase.data import atomic_numbers
 
 from asetui.app import ASETUI
 from asetui.table import AsetuiTable
-from asetui.filter import Filter
 
-from .shared_info import test_atoms, get_column_labels
+
+async def check_element_search(table, pilot, element):
+    """Check that the cursor is on the correct row, and that the row
+    id is correct.
+
+    """
+    await pilot.press("ctrl+s", *list(element), "enter")
+    assert table.cursor_row == atomic_numbers[element] - 1
+    assert str(table.get_cell_at(Coordinate(table.cursor_row, 0))) == str(
+        atomic_numbers[element]
+    )
 
 
 @pytest.mark.asyncio
@@ -14,8 +25,53 @@ async def test_search(big_db_path):
     app = ASETUI(path=big_db_path)
     async with app.run_test(size=(200, 50)) as pilot:
         searchbox = app.query_one("#search-box")
-        
+
         # Check status before adding filter
         assert not app.show_search_box
         assert not searchbox.display
-        
+
+        await pilot.press("ctrl+s")
+
+        # Check status after adding filter
+        assert app.show_search_box
+        assert searchbox.display
+
+        # Search for Ar and check that the search box is not visible
+        await pilot.press("A", "r", "enter")
+        assert not app.show_search_box
+        assert not searchbox.display
+
+        # The selected row should be 17 since Ar is the 18th element
+        # in the periodic table and the table is 0-indexed
+        table = app.query_one(AsetuiTable)
+        assert table.cursor_row == 17
+
+        # The row id, however, should be 18
+        assert str(table.get_cell_at(Coordinate(table.cursor_row, 0))) == "18"
+
+        # Check some elements
+        for element in ["Li", "Au", "Es"]:
+            await check_element_search(table, pilot, element)
+
+
+@pytest.mark.asyncio
+async def test_next_and_previous(big_db_path):
+    app = ASETUI(path=big_db_path)
+    async with app.run_test(size=(200, 50)) as pilot:
+        table = app.query_one(AsetuiTable)
+        await pilot.press("ctrl+s", "S")
+
+        # First hit for S should be Si
+        assert table.cursor_row == 13
+
+        await pilot.press("ctrl+s")
+        # Then S
+        assert table.cursor_row == 15
+
+        await pilot.press("ctrl+s")
+        # Then Sc
+        assert table.cursor_row == 20
+
+        await pilot.press("ctrl+r")
+        # Back to S
+        assert table.cursor_row == 15
