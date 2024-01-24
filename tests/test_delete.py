602196a5fdecef869927df43510eb0e20e6067f3
Steen Lysgaard <stly@dtu.dk>
2024-01-24 15:15:34 +0100

    test delete marked rows

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -23,3 +23,16 @@ async def test_delete(big_db_path):
         
         
         
+@pytest.mark.asyncio
+async def test_delete_marked_rows(big_db_path):
+    app = TEXASE(path=big_db_path)
+    async with app.run_test(size=(200, 50)) as pilot:
+        table = app.query_one(TexaseTable)
+        
+        # Mark a couple of rows
+        await pilot.press("down", "space", "space", "space", "#", "y")
+        assert app.data.df["id"].iloc[0] == 1
+        assert app.data.df["id"].iloc[1] == 5
+        for i in [2, 3, 4]:
+            with pytest.raises(KeyError):
+                assert connect(big_db_path).get(id=i)
