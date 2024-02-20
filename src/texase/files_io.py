c988926b6f68c8dc937cc3e5cde1f4f6ab5916ff
Steen Lysgaard <stly@dtu.dk>
2024-02-20 09:53:23 +0100

    build read extensions and globs together but prefer globs.
    This is only based on the vasp IOFormat that currently includes POSCAR
    and CONTCAR in globs but only poscar in extensions.

--- src/texase/files_io.py
+++ src/texase/files_io.py
@@ -21,45 +21,50 @@ def build_write_exts() -> set[str]:
                 ext_list.extend(format.extensions)
             else:
                 ext_list.append(format.name)
-    return set([f'.{ext}' for ext in ext_list])
+    return set([f".{ext}" for ext in ext_list])
+
 
 ASE_IO_WRITE_EXTS = build_write_exts()
 
-def build_read_exts() -> set[str]:
-    ext_list = []
-    for format in ioformats.values():
-        if format.can_read:
-            if format.extensions:
-                ext_list.extend(format.extensions)
-            else:
-                ext_list.append(format.name)
-    return set([f'.{ext}' for ext in ext_list])
 
-def build_read_globs() -> set[str]:
+def build_read_extensions_and_globs() -> tuple[set[str], set[str]]:
+    ext_list = []
     glob_list = []
     for format in ioformats.values():
         if format.can_read:
+            # prefer globs to extensions, this is only based on vasp IOFormat
             if format.globs:
                 glob_list.extend(format.globs)
-    return set(glob_list)
+            elif format.extensions:
+                ext_list.extend(format.extensions)
+            else:
+                ext_list.append(format.name)
+    return set([f".{ext}" for ext in ext_list]), set(glob_list)
 
-ASE_IO_READ_EXTS = build_read_exts()
-ASE_IO_READ_GLOBS = build_read_globs()
+
+ASE_IO_READ_EXTS, ASE_IO_READ_GLOBS = build_read_extensions_and_globs()
+
 
 class ASEWriteDirectoryTree(DirectoryTree):
     def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
-        return [path for path in paths
-                if (path.suffix in ASE_IO_WRITE_EXTS or path.is_dir())]
-    
+        return [
+            path
+            for path in paths
+            if (path.suffix in ASE_IO_WRITE_EXTS or path.is_dir())
+        ]
+
+
 class ASEReadDirectoryTree(DirectoryTree):
-    
-    BINDINGS = [Binding("left", "set_root_up", "Go up", show=False),
-                Binding("right", "set_root_down", "Go down", show=False)]
-    
+
+    BINDINGS = [
+        Binding("left", "set_root_up", "Go up", show=False),
+        Binding("right", "set_root_down", "Go down", show=False),
+    ]
+
     def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
         allowed_paths = []
         for path in paths:
-            if path.name.startswith('.'):
+            if path.name.startswith("."):
                 # Don't allow hidden files (on Unix defined as starting with a .)
                 continue
             elif path.suffix in ASE_IO_READ_EXTS or path.is_dir():
@@ -67,7 +72,7 @@ class ASEReadDirectoryTree(DirectoryTree):
         for glob in ASE_IO_READ_GLOBS:
             allowed_paths.extend(path.parent.glob(glob))
         return allowed_paths
-        
+
     def action_set_root_up(self) -> None:
         """If the root node is selected, set a new root node as the
         parent of the current root node."""
@@ -76,33 +81,31 @@ class ASEReadDirectoryTree(DirectoryTree):
                 self.path = self.path.parent
             elif self.cursor_node.data.path.is_dir():
                 self.select_node(self.cursor_node.parent)
-            
+
     def action_set_root_down(self) -> None:
         """If a directory is selected, set it as the new root node."""
         if self.cursor_node is not None and self.cursor_node.data.path.is_dir():
             self.path = self.cursor_node.data.path
             self.select_node(self.root)
-            
-
 
 
 class FilesIOScreen(ModalScreen[Path | None]):
     """Screen with a question that can be answered yes or no."""
-    
-#     DEFAULT_CSS = """
-        # FilesIOScreen {
-        #     align: center middle;
-        # }
 
-        # YesNoScreen > Label {
-        #     text-align: center;
-        #     width: 50%;
-        #     height: auto;
-        #     border: heavy $warning;
-        #     padding: 2 4;
-        # }
-# """
-    
+    #     DEFAULT_CSS = """
+    # FilesIOScreen {
+    #     align: center middle;
+    # }
+
+    # YesNoScreen > Label {
+    #     text-align: center;
+    #     width: 50%;
+    #     height: auto;
+    #     border: heavy $warning;
+    #     padding: 2 4;
+    # }
+    # """
+
     BINDINGS = [
         # ("y", "yes", "Yes"),
         # ("n", "no", "No"),
@@ -117,15 +120,19 @@ class FilesIOScreen(ModalScreen[Path | None]):
 
     def compose(self) -> ComposeResult:
         if self.read:
-            yield Container(Label('Select a file to read from'),
-                            ASEReadDirectoryTree(Path(".").resolve()),
-                            FolderLabel("Current folder"),
-                            Input())
+            yield Container(
+                Label("Select a file to read from"),
+                ASEReadDirectoryTree(Path(".").resolve()),
+                FolderLabel("Current folder"),
+                Input(),
+            )
         else:
-            yield Container(Label('Select a file to write to'),
-                            ASEWriteDirectoryTree(Path(".").resolve()),
-                            FolderLabel("Current folder"),
-                            Input())
+            yield Container(
+                Label("Select a file to write to"),
+                ASEWriteDirectoryTree(Path(".").resolve()),
+                FolderLabel("Current folder"),
+                Input(),
+            )
         yield Footer()
 
     # def action_yes(self) -> None:
@@ -133,23 +140,24 @@ class FilesIOScreen(ModalScreen[Path | None]):
 
     def action_cancel(self) -> None:
         self.dismiss(None)
-        
+
     def on_input_submitted(self, submitted: Input.Submitted) -> None:
         self.dismiss(Path(submitted.value))
 
-    def on_directory_tree_file_selected(self, selected: DirectoryTree.FileSelected) -> None:
+    def on_directory_tree_file_selected(
+        self, selected: DirectoryTree.FileSelected
+    ) -> None:
         self.dismiss(selected.path)
-        
+
     @on(Tree.NodeHighlighted)
     def set_input(self, event: Tree.NodeHighlighted) -> None:
         # Add a / to the end if it is a directory
         path = event.node.data.path
         str_path = str(path)
         if path.is_dir():
-            str_path += '/'
+            str_path += "/"
         self.query_one(Input).value = str_path
+
 
 class FolderLabel(Label):
     ...
-    
-
