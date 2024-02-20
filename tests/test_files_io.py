ff1109a177426d409ae66ea5fb6cf40c7de1f048
Steen Lysgaard <stly@dtu.dk>
2024-02-20 12:35:39 +0100

    test writing (export) of files

--- tests/test_files_io.py
+++ tests/test_files_io.py
@@ -6,8 +6,10 @@ from textual.coordinate import Coordinate
 from texase.app import TEXASE
 from texase.table import TexaseTable
 
-from ase.io import write
+from ase.io import write, read
 from ase import Atoms
+
+from .shared_info import test_atoms
 
 
 @pytest.mark.asyncio
@@ -57,3 +59,37 @@ async def test_read_bad_file(db_path, tmp_path):
         assert len(table.rows.keys()) == original_no_rows
 
         
+@pytest.mark.asyncio
+async def test_write_traj_file(db_path, tmp_path):
+    app = TEXASE(path=db_path)
+    async with app.run_test(size=(200, 50)) as pilot:
+        fname = str(tmp_path / 'test.traj')
+        
+        # Export a trajectory file of the first row
+        await pilot.press("x", "tab", "ctrl+u", *list(fname), "enter")
+        
+        # Check that the file has been created
+        assert Path(fname).exists()
+        atoms = read(fname)
+        
+        assert atoms.get_chemical_symbols() == [test_atoms[0]]
+
+@pytest.mark.asyncio
+async def test_write_bad_file(db_path, tmp_path):
+    app = TEXASE(path=db_path)
+    async with app.run_test(size=(200, 50)) as pilot:
+        # Try to write a silly file
+        fname = str(tmp_path / 'foo.bar')
+        
+        # Check that no error messages are displayed
+        assert len(app._notifications) == 0
+        
+        # Import the trajectory file
+        await pilot.press("x", "tab", "ctrl+u", *list(fname), "enter")
+        
+        # Check that the error message is displayed
+        await pilot.pause()
+        assert len(app._notifications) == 1
+        
+        # Check that the file has not been created
+        assert not Path(fname).exists()
